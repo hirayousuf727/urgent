289af2182b2991e943fc18722fb6d255a85c599c
hira yousuf <gyousufghori@gmail.com>
2024-12-08 08:17:28 -0800

    Add files via upload
    sir i doone my urgent assignnmnet

---
+++ app.js
@@ -0,0 +1,21 @@
+var temp1= prompt("enter temp1")
+var temp2= prompt("enter temp2")
+var temp3= prompt("enter temp3")
+var temp4= prompt("enter temp4")
+
+if (temp1>40 || temp2>40  || temp3>40 || temp4>40 )
+console.log("it's too hot outside")
+
+
+else if (temp1>30 || temp2 >30  || temp3>30 || temp4>30 )
+    console.log("the wheather today is normal ")
+
+
+else if (temp1>20 || temp2>20 || temp3>20 || temp4>20 )
+    console.log("tday wheather is  cool ")
+
+else if (temp1>10  || temp2>10 || temp3>10 || temp4>10 )
+    console.log("OMG! today wheather is so cool")
+else (temp1<10  || temp2<10 || temp3<10 || temp4<10)
+console.log("plz write a number greater than 10")
+
